17e184d25db8a3c3e1b425c2702e416907624bab
justindaerolee <daero1995lee@gmail.com>
2017-05-15 16:31:35 +1000

    Update bfs.c

--- bfs.c
+++ bfs.c
@@ -4,22 +4,18 @@
 #include "bbq.h"
 #include "bfs.h"
 
-typedef struct llist {
-	llist next;
-	Tree node;
-} *llist
-
 // change to c code.
 void search (Tree root, unsigned int N, Key key, T *val, RetVal *found) {
+	List *q = (List*)malloc(sizeof(List));
 	int n = 0;
-	llist q = (llist*)malloc(sizeof(llist));;
 	q.add(root);
+	n ++;
 	while (n != 0 && *found == 0) {
 		Tree tmp = q.dequeue();
 		n --;
-		if (q->node->key == key) {
+		if (q->t->key == key) {
 			*found = 1;
-			*val = q->node->payload;
+			*val = q->t->val;
 		} else {
 			for child in children :
 				q.add(child);
