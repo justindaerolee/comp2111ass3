1e21e10e7f0447372e883a2a1e7505009f171f9b
justindaerolee <daero1995lee@gmail.com>
2017-05-15 16:19:04 +1000

    Add files via upload

---
+++ bfs.c
@@ -0,0 +1,31 @@
+//bfs for a tree
+
+#include <stdio.h>
+#include "bbq.h"
+#include "bfs.h"
+
+typedef struct llist {
+	llist next;
+	Tree node;
+} *llist
+
+// change to c code.
+void search (Tree root, unsigned int N, Key key, T *val, RetVal *found) {
+	llist q = (llist*)malloc(sizeof(llist));;
+	q.add(root);
+	while (n != 0 && *found == 0) {
+		Tree tmp = q.dequeue();
+		n --;
+		if (q->node->key == key) {
+			*found = 1;
+			*val = q->node->payload;
+		} else {
+			for child in children :
+				q.add(child);
+		}
+	}
+}
+
+// abstract bounded queue, q
+// length of the queue, n
+// match key and return val and found. 
