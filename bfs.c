7d2723ea37be1c2d9abe4c243fa15da9310637e2
Unknown <daero1995lee@gmail.com>
2017-05-16 16:59:33 +1000

    update

--- bfs.c
+++ bfs.c
@@ -6,35 +6,45 @@
 
 // change to c code.
 void search (Tree root, unsigned int N, Key key, T *val, RetVal *found) {
-	//initialisation
-	List *q = (List*)malloc(sizeof(List));
-	int n = 0;
-
-	// add first node
-	q->n = root;
-	q->next = NULL;
-	n ++;
-	//
-	while (n != 0 && *found == 0) {
-		//dequeue
-		Tree tmp = q->n;
-		q = q->next;
-		n --;
-		// if found no need to continue the search
-		if (tmp->id == key) {
-			*found = 1;
-			*val = tmp->val;
-		} else {
-			// else add all the children of the tmp tree.
-			List *children = tmp->list;
-			while (children != NULL) {
-				q.add(child_tree);
-				n++
-				children = children->next;
-			}
-		}
-	}
+    //initialisation
+    List *q = (List*)malloc(sizeof(List));
+    int n = 0;
+    
+    // add first node
+    q->n = root;
+    q->next = NULL;
+    n ++;
+    //
+    while (n != 0 && *found == 0) {
+        //dequeue
+        Tree tmp = q->n;
+        q = q->next;
+        n --;
+        // if found no need to continue the search
+        if (tmp->id == key) {
+            *found = 1;
+            *val = tmp->val;
+        } else {
+            // else add all the children of the tmp tree.
+            List *children = tmp->list;
+            while (children != NULL) {
+                add(q, children, n);
+                n++
+                children = children->next;
+            }
+        }
+    }
 }
+
+void enq(List *q, Tree v, int n) {
+    List *tmp = q;
+    int i = 1;
+    while (i != n) {
+        tmp = tmp->next;
+    }
+    tmp->next = v;
+}
+
 
 // abstract bounded queue, q
 // length of the queue, n
